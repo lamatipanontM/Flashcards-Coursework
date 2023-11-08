1e78703b02280f2a715078dee3c2b595179a8686
lamatipanontM <lamatipanont.m@oundleschool.org.uk>
2023-11-08 12:06:38 +0000

    start flash

--- flash.php
+++ flash.php
@@ -1,4 +1,36 @@
-<?php
-session_start();
-print_r($_SESSION["deck"]);
-?>
+<html>
+    <head>
+        <link rel="stylesheet" href="styles.css">
+        <title>Test Page</title>
+
+
+    </head>
+    <body>
+<div id="box">
+    
+    <?php
+    session_start();
+    //print_r($_SESSION["deck"]);
+
+    $deck = $_SESSION["deck"];
+    foreach ($deck as $card){
+        echo($card[0]."-".$card[1]);
+        echo("<br>");
+    }
+    $cardno=rand(0,2);
+    $card = $_SESSION["deck"][$cardno][0];
+    print($card);
+    ##print_r($deck[0]);
+
+
+        ?>
+        
+</div>
+
+
+
+</body>
+
+
+</html>
+
